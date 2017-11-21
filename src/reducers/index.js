a5ad187cbfe44eca1e186d21fd6a9f6d29cd1bea
chrispauley <pauleyc@yahoo.com>
2017-11-21 15:24:07 -0500

    Updates

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import storiesReducer from './storiesReducer'
-import userReducer from './userReducer'
-import beersReducer from './beersReducer'
-
-export default combineReducers({
-    storiesReducer, userReducer, beersReducer
-})
